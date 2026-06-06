466bddbf68fe25e695f5d4e6833c4675d57cf3ba
Tanvi Kumari <tanvisharma5485@gmail.com>
2026-06-06 22:42:17 +0530

    streamlit setup + homescreen

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+from src.screens.home_screen import home_screen
+from src.screens.student_screen import student_screen
+from src.screens.teacher_screen import teacher_screen
+
+
+
+def main():
+    
+    if 'login-type' not in st.session_state:
+        st.session_state['login-type'] = None
+
+    match st.session_state['login-type']:
+        case 'teacher':
+            teacher_screen()
+        case 'student':
+            student_screen()
+        case None:
+            home_screen()
+    
+
+
+main()
---
+++ src/components/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def footer_home():
+    st.markdown(f"""
+        <div style="margin-top: 2rem; display:flex; gap: 6px; justify-content: center; items-align: center">
+                <p style="font-weight: bold; color:white;">Created with ♥️ by Tanvi</p>
+        </div>
+
+
+        """, unsafe_allow_html=True)
