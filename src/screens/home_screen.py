37c1fce328fa835b05c33964e2b3f8114bb15895
Tanvi Kumari <tanvisharma5485@gmail.com>
2026-06-13 21:06:31 +0530

    typing error

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -14,14 +14,14 @@ def home_screen():
         st.header("I'm Student")
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
         if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login-type'] = 'student'
+            st.session_state['login_type'] = 'student'
             st.rerun()
 
     with col2:
         st.header("I'm Teacher")
         st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=153)
         if st.button('Teacher Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login-type'] ='teacher'
+            st.session_state['login_type'] ='teacher'
             st.rerun()
 
 
