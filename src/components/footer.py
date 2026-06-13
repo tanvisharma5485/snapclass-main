58d224853bf4006cb1e31e4f724e9f6295cfdf01
Tanvi Kumari <tanvisharma5485@gmail.com>
2026-06-13 17:44:32 +0530

    finishing

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,10 +1,21 @@
 import streamlit as st
 
+
 def footer_home():
+    
     st.markdown(f"""
-        <div style="margin-top: 2rem; display:flex; gap: 6px; justify-content: center; items-align: center">
-                <p style="font-weight: bold; color:white;">Created with ♥️ by Tanvi</p>
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by Tanvi</p>  
         </div>
+                
+                """, unsafe_allow_html=True)
+    
 
-
-        """, unsafe_allow_html=True)
+def footer_dashboard():
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by Tanvi</p>  
+        </div>
+                
+                """, unsafe_allow_html=True)
